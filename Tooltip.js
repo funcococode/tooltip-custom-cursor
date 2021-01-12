54ed2161a95167082725141db9f0f6666ab9f120
funcococode <funcococode@gmail.com>
2021-01-13 00:12:32 +0530

    added more styling options

--- Tooltip.js
+++ Tooltip.js
@@ -1,33 +1,56 @@
 const tooltipContainers = document.querySelectorAll('.tooltip');
 
-export default class Tooltip{
-    constructor(config){
+export default class Tooltip {
+    constructor(config) {
         this.color = config.color || "#fff";
         this.bgColor = config.bgColor || "#000";
         this.rolling = config.rolling || false;
+        this.position = config.position || "top left";
+        this.positionArr = this.position.split(" ");
+        this.rounded = config.rounded || false;
     }
 
-    createTooltip(){
-        tooltipContainers.forEach(container=>{
+    createTooltip() {
+        tooltipContainers.forEach(container => {
             let span = document.createElement("span");
             container.appendChild(span);
             span.classList.add("popup");
-            span.style.backgroundColor = this.bgColor;
-            span.style.color = this.color;
+            span.style.backgroundColor = (this.bgColor == "random") ? this.createRandomColor() : this.bgColor;
+            span.style.color = (this.color == "random") ? this.createRandomColor() : this.color;
+            span.style.borderRadius = (this.rounded)?"100px":"0";
+            span.style.transform = (this.positionArr[0] == "top") ? "translateY(-100%)" : "translateY(100%)";
+            span.style.left = (this.positionArr[1] == "left") ? "0" : "100%";
             
+            if(this.positionArr.length == 1){
+                span.style.transform = "translateY(0%)";
+                span.style.left = (this.positionArr[0] == "left") ? "0" : "100%";
+                
+                if(this.positionArr[0] == "top" || this.positionArr[0] == "bottom"){
+                    span.style.transform = (this.positionArr[0] == "top") ? "translateY(-100%)" : "translateY(100%)";
+                    span.style.left = "0";
+                }
+            }
+
             let toolTip = container.getAttribute("data-tooltip");
             let finalTooltip = (this.rolling) ? `<marquee>${toolTip}</marquee>` : toolTip;
             span.innerHTML = finalTooltip;
-            
-            
-            container.addEventListener("mouseover",()=>{
+
+
+            container.addEventListener("mouseover", () => {
                 span.classList.add("visible");
             });
-            container.addEventListener("mouseout",()=>{
+            container.addEventListener("mouseout", () => {
                 span.classList.remove("visible");
             });
         });
     }
 
-}
 
+    createRandomColor() {
+        let h = Math.floor(Math.random() * 360);
+        let s = Math.floor(Math.random() * 100);
+        let l = Math.floor(Math.random() * 100);
+
+        return `hsl(${h},${s}%,${l}%)`;
+    }
+}
--- main.js
+++ main.js
@@ -1,101 +1,10 @@
-<<<<<<< HEAD
-let pointer = document.querySelector("#circle");
-let tooltipElements = document.querySelectorAll("h1[data-tooltip]");
-tooltipElements.forEach(elem => {
-    let span = document.createElement("span");
-    elem.appendChild(span);
-    elem.addEventListener("mouseover",(e)=>{
-        let popupText = e.target.getAttribute("data-tooltip");
-        span.classList.add("popup");
-        span.innerHTML = `${popupText}`;
-        span.classList.add("visible");
-        span.style.left = -(elem.getBoundingClientRect().x - elem.firstChild.getBoundingClientRect().width) + "px";
-    });
+import Tooltip from "/Tooltip.js";
 
-    elem.addEventListener("mouseout",(e)=>{
-        span.classList.remove("visible");
-    });
+let tooltip = new Tooltip({
+    color:"pink",
+    bgColor:"red",
+    rolling:true,
+    position : "top left",
+    rounded: true
 });
-
-addEventListener("mousemove",(e)=>{
-    move(e.x,e.y);
-    if(e.target.getAttribute("data-tooltip")){
-        let targetCoords = {
-            x : e.target.getBoundingClientRect().left,
-            y : e.target.getBoundingClientRect().top,
-            width : e.target.getBoundingClientRect().width,
-            height : e.target.getBoundingClientRect().height
-        }; 
-        pointer.style.transition = "min-width ease 0.25s , min-height ease 0.21s  , background ease 0.25s";
-        pointer.style.left = (targetCoords.x) + "px";
-        pointer.style.top = (targetCoords.y) + "px";
-        pointer.style.background = "none";
-        pointer.style.border = "1px solid black";
-        pointer.style.minWidth = targetCoords.width + "px";
-        pointer.style.minHeight = targetCoords.height + "px";
-        pointer.style.zIndex = "1";
-
-        
-    }else{
-        pointer.style.background = "black";
-        pointer.style.border = "none";
-        pointer.style.transition = "min-width ease 0.25s , min-height ease 0.21s, background ease 0.25s ";
-        pointer.style.minWidth = "30px";
-        pointer.style.minHeight = "30px";
-    }
-    
-});
-
-function move(x,y){
-    pointer.style.left = (x ) + "px";
-    pointer.style.top = (y ) + "px";
-=======
-let pointer = document.querySelector("#circle");
-let tooltipElements = document.querySelectorAll("h1[data-tooltip]");
-tooltipElements.forEach(elem => {
-    elem.addEventListener("mouseover",(e)=>{
-        let popupText = e.target.getAttribute("data-tooltip");
-        elem.firstChild.textContent = popupText;
-        elem.firstChild.classList.add("visible")
-        elem.firstChild.style.left = -(elem.getBoundingClientRect().x - elem.firstChild.getBoundingClientRect().width) + "px"
-    });
-
-    elem.addEventListener("mouseout",(e)=>{
-        elem.firstChild.classList.remove("visible")
-    })
-})
-
-addEventListener("mousemove",(e)=>{
-    move(e.x,e.y)
-    if(e.target.getAttribute("data-tooltip")){
-        let targetCoords = {
-            x : e.target.getBoundingClientRect().left,
-            y : e.target.getBoundingClientRect().top,
-            width : e.target.getBoundingClientRect().width,
-            height : e.target.getBoundingClientRect().height
-        } 
-        pointer.style.transition = "min-width ease 0.25s , min-height ease 0.21s  , background ease 0.25s";
-        pointer.style.left = (targetCoords.x) + "px";
-        pointer.style.top = (targetCoords.y) + "px";
-        pointer.style.background = "none";
-        pointer.style.border = "1px solid black"
-        pointer.style.minWidth = targetCoords.width + "px";
-        pointer.style.minHeight = targetCoords.height + "px";
-        pointer.style.zIndex = "1";
-
-        
-    }else{
-        pointer.style.background = "black";
-        pointer.style.border = "none"
-        pointer.style.transition = "min-width ease 0.25s , min-height ease 0.21s, background ease 0.25s ";
-        pointer.style.minWidth = "30px";
-        pointer.style.minHeight = "30px"
-    }
-    
-})
-
-function move(x,y){
-    pointer.style.left = (x ) + "px";
-    pointer.style.top = (y ) + "px";
->>>>>>> 9f1f62ed7b99f96de8b996b7d6f4a3d1ec6984d7
-}
+tooltip.createTooltip();
